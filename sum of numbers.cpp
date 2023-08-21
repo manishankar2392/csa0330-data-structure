140c7fc02443955b6a3155421501ebccb4f50b80
manishankar2392 <114464138+manishankar2392@users.noreply.github.com>
2023-08-21 13:37:38 +0530

    Create sum of numbers.cpp

---
+++ sum of numbers.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,sum;
+	printf("enter the number:");
+	scanf("%d",&a);
+	printf("enter the number:");
+	scanf("%d",&b);
+	sum=a+b;
+	printf("%d=sum",sum);
+}
