3f6e6496f194bfba3ffc654f4e0eb1b492298677
manishankar2392 <114464138+manishankar2392@users.noreply.github.com>
2023-08-21 13:38:05 +0530

    Create swapping of two number.c

---
+++ swapping of two number.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,t;
+	a=10;
+	b=30;
+	printf("before swapping : a = %d b = %d",a,b);
+	t=a;
+	a=b;
+	b=t;
+	printf("before swapping : a = %d b = %d",a,b);
+}
